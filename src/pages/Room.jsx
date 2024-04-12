ce52089a996d62fd509715a7bec8e7cc4c3a7151
Bhavadeep Bhukya <bhavadeepbhukya@Bhavadeeps-MacBook-Air.local>
2024-04-12 15:35:06 +0530

    added handling frnds

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -13,29 +13,47 @@ import { Trash } from 'react-feather'
 import { ChevronLeft, ChevronRight, UserPlus, Send, LogOut } from 'react-feather'
 import client from "../appwriteConfig";
 import { useAuth } from "../utils/AuthContext";
+import { data } from "autoprefixer";
+import { ChevronsLeft, ChevronsRight, Check, X } from "react-feather";
 
 function Room() {
-  const {user,handleUserLogin,handleUserLogout} = useAuth()
+  const { user, handleUserLogin, handleUserLogout } = useAuth()
   // console.log('user id:',user.$id)
   const [messagesByMe, setMessagesByMe] = useState([]);
+  const [profileInfo, setProfileInfo] = useState(null);
   const [messagesToMe, setMessagesToMe] = useState([]);
   const [messageBody, setMessageBody] = useState("");
-  const [profile, setProfile] = useState(null);
-  const [friendsUserIds, setFriendsUserIds] = useState([]);
-  const [friends,setFriends] = useState([]);
+  const [friends, setFriends] = useState([]);
+  const [username, setUsername] = useState('');
+  const [userTag, setUserTag] = useState('');
+  const [reqFriendUserName, setReqFriendUserName] = useState('');
+  const [reqFriendTag, setReqFriendTag] = useState('');
+  const [friendRequests, setFriendReqests] = useState([]);
+
+  const [currFrndReqUsername, setCurrFrndReqUsername] = useState('');
+  const [currFrndReqIdx, setCurrFrndReqIdx] = useState(-1);
+  const [currFrndReqTag, setCurrFrndReqTag] = useState('');
+  const [currFrndReqId, setCurrFrndReqId] = useState('');
+
+  const [hasFrndReq, setHasFrndReq] = useState(false);
+
   useEffect(() => {
     getMessages();
     getProfileData();
-    const unsubscribe =client.subscribe(`databases.${DATABASEID}.collections.${COLLECTIONID_MESSAGES}.documents`,response=>{
-      if(response.events.includes('databases.*.collections.*.documents.*.create')){
-        setMessagesByMe(prevState=>[...prevState,response.payload]);
+    getUserInfo();
+    getUserFrndReqs();
+    // intialiseFrndReq();
+    // console.log('proflie data in useEffect1:',profileInfo)
+    const unsubscribe = client.subscribe(`databases.${DATABASEID}.collections.${COLLECTIONID_MESSAGES}.documents`, response => {
+      if (response.events.includes('databases.*.collections.*.documents.*.create')) {
+        setMessagesByMe(prevState => [...prevState, response.payload]);
       }
-      if(response.events.includes('databases.*.collections.*.documents.*.delete')){
+      if (response.events.includes('databases.*.collections.*.documents.*.delete')) {
         setMessagesByMe(messagesByMe.filter(message => message.$id !== response.payload.$id));
         setMessagesToMe(messagesByMe.filter(message => message.$id !== response.payload.$id));
       }
     });
-    return()=>{
+    return () => {
       unsubscribe();
     }
   }, []);
@@ -62,37 +80,101 @@ function Room() {
 
   };
 
-  const getProfileData = async () =>{
-    // const response = await databases.listDocuments(
-    //   DATABASEID,
-    //   COLLECTIONID_PROFILES,
-    //   [
-    //     Query.equal('userId',user.$id)
-    //   ]
-    // )
-    // setFriendsUserIds(response.documents[0]['friends'])
-    // console.log("My profile:", response)
-    // console.log("My friends:", friendsUserIds)
-    // await Promise.all(friendsUserIds.map(async (friendsUserId)=>{
-    //   const friendUserResponse = await databases.listDocuments(
-    //     DATABASEID,
-    //     COLLECTIONID_PROFILES,
-    //     [
-    //       Query.equal('userId',friendsUserId)
-    //     ]
-    //   )
-    //   friends.push(friendUserResponse)
-    // }))
-    // console.log(friends)
+  const handleSendReq = async (e) => {
+    // getProfileData();
+    e.preventDefault();
+    try {
+      if (reqFriendTag.length !== 0 && reqFriendUserName.length !== 0) {
+        console.log('reqFriendTag', reqFriendTag)
+        console.log('reqFriendUserName', reqFriendUserName)
+        const response = await databases.listDocuments(
+          DATABASEID,
+          COLLECTIONID_PROFILES,
+          [
+            Query.equal('username', reqFriendUserName),
+            Query.equal('tag', reqFriendTag),
+          ]
+        )
+        let frndRequestUserId = response['documents'][0].$id
+        let frndprofileId = response['documents'][0]['userId']
+        let alreadFriend = false
+        for(let i=0;i<friends.length;i++){
+          if(friends[i]['documents'][0]['userId']===frndprofileId){
+            alreadFriend=true
+            console.log("already your friend")
+          }
+        }
+        console.log("is he already a friend:",alreadFriend)
+        if (frndRequestUserId && !alreadFriend) {
+          const doc = await databases.getDocument(
+            DATABASEID,
+            COLLECTIONID_PROFILES,
+            frndRequestUserId
+          )
+          console.log('retrieved doc before:', doc)
+          doc['FriendReqs'].push(user.$id);
+          console.log('retrieved doc after:', doc)
+          const updateResponse = await databases.updateDocument(
+            DATABASEID,
+            COLLECTIONID_PROFILES,
+            frndRequestUserId,
+            {
+              'FriendReqs': doc['FriendReqs']
+            }
+          )
+        }
+        else{
+          console.log('not sent any frnd req');
+        }
+        console.log('friendReq:', frndRequestUserId)
+        setReqFriendTag('')
+        setReqFriendUserName('')
+      }
+      else {
+        console.log('fill details properly')
+      }
+
+    }
+    catch (error) {
+      console.log("error while sending friend req:", error);
+    }
+  }
+
+  const getUserInfo = async () => {
+    // console.log("working getUserInfo")
+    try {
+      const response = await databases.listDocuments(
+        DATABASEID, COLLECTIONID_PROFILES, [
+        Query.equal('userId', user.$id)
+      ]
+      )
+      setUsername(response['documents'][0]['username'])
+      // console.log('userInfo:',username)
+      setUserTag(response['documents'][0]['tag'])
+      // console.log('userInfo:',userTag)
+
+
+    } catch (error) {
+      console.log("error while fetching getUserInfo")
+    }
+  }
+
+  const getProfileData = async () => {
+
     await databases.listDocuments(
       DATABASEID,
       COLLECTIONID_PROFILES,
       [
-        Query.equal('userId',user.$id)
+        Query.equal('userId', user.$id)
       ]
     ).then(
-      response=>{
-        console.log("user profile:",response)
+      response => {
+        console.log("user profile:", response)
+        // setProfileInfo(response)
+        // setUsername(response['documents'[0]['username']])
+        // console.log('username:',response['documents'][0]['username'])
+
+        // console.log('profileData:',profileInfo)
         const friendPromises = response.documents[0]['friends'].map(async (friend) => {
           return await databases.listDocuments(
             DATABASEID,
@@ -100,16 +182,19 @@ function Room() {
             [Query.equal('userId', friend)]
           );
         });
-      
+
         Promise.all(friendPromises).then(
-          response=>{
-            console.log("friendsList:",response)
+          response => {
+            console.log("friendsList:", response)
             setFriends(response);
           }
         )
-        console.log("final friends:",friends)
+        // console.log("final friends:", friends)
+
+
       }
-    )
+    );
+
   }
 
   const getMessages = async () => {
@@ -117,16 +202,16 @@ function Room() {
       DATABASEID,
       COLLECTIONID_MESSAGES,
       [
-        Query.equal('from_user',user.$id),
+        Query.equal('from_user', user.$id),
         // Query.equal('to_user',user.$id)
       ]
     );
-    console.log(response);
+    // console.log(response);
     const response2 = await databases.listDocuments(
       DATABASEID,
       COLLECTIONID_MESSAGES,
       [
-        Query.equal('to_user',user.$id),
+        Query.equal('to_user', user.$id),
       ]
     )
     setMessagesByMe(response.documents);
@@ -139,11 +224,169 @@ function Room() {
   };
 
   const handleKeyDown = (event) => {
-    if (event.key === 'Enter' ) {
+    if (event.key === 'Enter') {
       event.preventDefault();
       handleSubmit();
     }
   };
+
+  // friend requests handling from here
+  const getUserFrndReqs = async () => {
+    const response = await databases.listDocuments(
+      DATABASEID,
+      COLLECTIONID_PROFILES,
+      [
+        Query.equal('userId', user.$id)
+      ]
+    )
+    // setFriendReqests(response['documents'][0]['FriendReqs']);
+    let friendReqUserIds = response['documents'][0]['FriendReqs'];
+    // console.log('friendReqsIds:', response['documents'][0]['FriendReqs']);
+    if (friendReqUserIds.length !== 0) {
+      const friendReqPromises = friendReqUserIds.map(async (friendreq) => {
+        return await databases.listDocuments(
+          DATABASEID,
+          COLLECTIONID_PROFILES,
+          [
+            Query.equal('userId', friendreq)
+          ]
+        );
+      });
+      Promise.all(friendReqPromises).then(
+        response => {
+          setFriendReqests(response)
+          if (response) {
+            setHasFrndReq(true)
+            setCurrFrndReqIdx(0);
+            setCurrFrndReqId(response[0]['documents'][0]['userId'])
+            setCurrFrndReqTag(response[0]['documents'][0]['tag'])
+            setCurrFrndReqUsername(response[0]['documents'][0]['username'])
+            console.log("intialised friendreqs")
+          }
+          console.log('friendReqs:', response);
+        }
+      )
+    }
+    console.log('has frnd req:', hasFrndReq)
+  }
+
+  const handlePrevReq = (e) => {
+    e.preventDefault();
+
+    if (currFrndReqIdx > 0 && friendRequests.length > 0) {
+      // console.log('left:',currFrndReqIdx-1,friendRequests[currFrndReqIdx-1]['documents'][0]['userId'])
+      setCurrFrndReqId(friendRequests[currFrndReqIdx - 1]['documents'][0]['userId']);
+      setCurrFrndReqUsername(friendRequests[currFrndReqIdx - 1]['documents'][0]['username']);
+      setCurrFrndReqTag(friendRequests[currFrndReqIdx - 1]['documents'][0]['tag']);
+      setCurrFrndReqIdx(currFrndReqIdx - 1);
+    }
+
+  }
+
+  const handleNextReq = (e) => {
+    e.preventDefault();
+
+    if (currFrndReqIdx < friendRequests.length - 1 && friendRequests.length > 0) {
+      // console.log('right:',currFrndReqIdx+1,friendRequests[currFrndReqIdx+1]['documents'][0]['userId'])
+      setCurrFrndReqId(friendRequests[currFrndReqIdx + 1]['documents'][0]['userId']);
+      setCurrFrndReqUsername(friendRequests[currFrndReqIdx + 1]['documents'][0]['username']);
+      setCurrFrndReqTag(friendRequests[currFrndReqIdx + 1]['documents'][0]['tag']);
+      setCurrFrndReqIdx(currFrndReqIdx + 1);
+    }
+  }
+
+  const handleAddFriend = async (e) => {
+    e.preventDefault();
+    const docs = await databases.listDocuments(
+      DATABASEID,
+      COLLECTIONID_PROFILES,
+      [
+        Query.equal('userId', user.$id)
+      ]
+    )
+    console.log('got profileId', docs)
+    let doc = docs['documents'][0]
+    let leftOverReqs = [];
+    for (let i = 0; i < doc['FriendReqs'].length; i++) {
+      if (doc['FriendReqs'][i] != currFrndReqId) {
+        leftOverReqs.push(doc['FriendReqs'][i]);
+      }
+    }
+    // setFriendReqests(leftOverReqs)
+    console.log("friend reqs after adding:",leftOverReqs);
+    if (leftOverReqs.length === 0) {
+      setHasFrndReq(false);
+    }
+    let currFriends = doc['friends'];
+    currFriends.push(currFrndReqId);
+    const updateResponse = await databases.updateDocument(
+      DATABASEID,
+      COLLECTIONID_PROFILES,
+      doc.$id,
+      {
+        'FriendReqs': leftOverReqs,
+        'friends': currFriends
+      }
+    )
+    console.log('updated friendReqs', updateResponse)
+
+    const docs2 = await databases.listDocuments(
+      DATABASEID,
+      COLLECTIONID_PROFILES,
+      [
+        Query.equal('userId', currFrndReqId),
+      ]
+    )
+    let doc2 = docs2['documents'][0]
+    console.log('got profileId2', docs2)
+
+    let hisFriends = doc2['friends']
+    console.log('his friends', hisFriends);
+    hisFriends.push(user.$id);
+    const updateResponse2 = await databases.updateDocument(
+      DATABASEID,
+      COLLECTIONID_PROFILES,
+      doc2.$id,
+      {
+        'friends': hisFriends,
+      }
+    )
+    console.log('updated friends', updateResponse2)
+    getUserFrndReqs();
+    getProfileData();
+  }
+
+  const handleDeleteReq = async(e)=>{
+    e.preventDefault();
+    const docs = await databases.listDocuments(
+      DATABASEID,
+      COLLECTIONID_PROFILES,
+      [
+        Query.equal('userId', user.$id)
+      ]
+    )
+    let doc = docs['documents'][0]
+    let leftOverReqs = [];
+    for (let i = 0; i < doc['FriendReqs'].length; i++) {
+      if (doc['FriendReqs'][i] != currFrndReqId) {
+        leftOverReqs.push(doc['FriendReqs'][i]);
+      }
+    }
+    // setFriendReqests(leftOverReqs)
+    console.log("friend reqs after adding:",leftOverReqs);
+    if (leftOverReqs.length === 0) {
+      setHasFrndReq(false);
+    }
+    const updateResponse = await databases.updateDocument(
+      DATABASEID,
+      COLLECTIONID_PROFILES,
+      doc.$id,
+      {
+        'FriendReqs': leftOverReqs
+      }
+    )
+    getUserFrndReqs();
+  }
 
   return (
     <>
@@ -190,42 +433,79 @@ function Room() {
               <img src="https://iili.io/JNkdzyx.png" alt="male user"></img>
             </div>
             <div className="user-name">
-              Bhavadeep
+              {username}
             </div>
-            <LogOut color="white" cursor="pointer" onClick={handleUserLogout}/>
+            <div className="user-tag">
+              #{userTag}
+            </div>
+
+            <LogOut color="white" cursor="pointer" onClick={handleUserLogout} />
           </div>
           {/* friends */}
           <div className="friends-section">
             <h3>Friends</h3>
           </div>
-          {friends.map((friend)=>(
+          {friends.map((friend) => (
             <div className="friendListContent" id={friend['documents'][0].$id}>
-            <div className="box">
-              <div className="user-icon">
-                <img src="https://iili.io/JNkdzyx.png" alt="male user"></img>
-              </div>
-              <div className="user-name">
-                {friend['documents'][0]['username']}
-              </div>
-              <div className="user-tag">
-              #{friend['documents'][0]['tag']}
+              <div className="box">
+                <div className="user-icon">
+                  <img src="https://iili.io/JNkdzyx.png" alt="male user"></img>
+                </div>
+                <div className="user-name">
+                  {friend['documents'][0]['username']}
+                </div>
+                <div className="user-tag">
+                  #{friend['documents'][0]['tag']}
+                </div>
               </div>
             </div>
-          </div>
           ))}
+          {/* Friend Requests */}
+          <div className="friend-requests">
+          <div className="friend-req-header">
+              <h3>Friend Requests</h3>
+            </div>
+            {hasFrndReq ? (<><div className="frnd-req-wrapper">
+                <div className="pointerOnHover">
+                  <ChevronsLeft color="white" onClick={handlePrevReq} />
+                </div>
+                <div className="friend-req-info">
+                  <div className="user-info">
+                    <div className="friend-req-name">
+                      {currFrndReqUsername}
+                    </div>
+                    <div className="friend-req-tag">
+                      #{currFrndReqTag}
+                    </div>
+                  </div>
+                  <div className="accept-decline">
+                    <div className="pointerOnHover">
+                      <X color="red" onClick={handleDeleteReq}/>
+                    </div>
+                    <div className="pointerOnHover">
+                      <Check color="green" onClick={handleAddFriend} />
+                    </div>
+                  </div>
+                </div>
+                <div className="pointerOnHover">
+                  <ChevronsRight color="white" onClick={handleNextReq} />
+                </div>
+              </div></>) : (<h1>EMPTY</h1>)}
+          </div>
+
           {/* add-friend section */}
 
           <div className="addFriend">
             <div className="input-container">
-              <input type="text" className="input-field" placeholder="User Id" />
+              <input type="text" className="input-field" placeholder="User Id" onChange={(e) => { setReqFriendUserName(e.target.value) }} value={reqFriendUserName} />
               <div className="line"></div>
             </div>
             <div className="input-container">
-              <input type="text" className="input-field" placeholder="Hashtag" />
+              <input type="text" className="input-field" placeholder="Hashtag" onChange={(e) => { setReqFriendTag(e.target.value) }} value={reqFriendTag} />
               <div className="line"></div>
             </div>
             <div className="addbutton">
-              <UserPlus color="white" />
+              <UserPlus onClick={handleSendReq} color="white" />
             </div>
           </div>
         </div>
